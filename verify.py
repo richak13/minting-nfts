cc5ea52c67c9700d687c3eeda69f494b9ddd97fe
richak13 <rkhed@seas.upenn.edu>
2024-11-03 17:14:17 -0500

    Update verify.py

--- verify.py
+++ verify.py
@@ -6,14 +6,14 @@ def signChallenge( challenge ):
 
     w3 = Web3()
 
-    #This is the only line you need to modify
-    sk = "YOUR SECRET KEY HERE"
+    sk = "1cde024c70d3deff5b8bec33aba4d14f542be3200f53ffc4853df6fdaa475a8a"
 
     acct = w3.eth.account.from_key(sk)
 
-    signed_message = w3.eth.account.sign_message( challenge, private_key = acct._private_key )
+    signed_message = acct.sign_message(challenge)
 
     return acct.address, signed_message.signature
+
 
 
 def verifySig():
@@ -21,15 +21,16 @@ def verifySig():
         This is essentially the code that the autograder will use to test signChallenge
         We've added it here for testing 
     """
-
     challenge_bytes = random.randbytes(32)
-
     challenge = encode_defunct(challenge_bytes)
-    address, sig = signChallenge( challenge )
+    
+    # Call signChallenge to sign the challenge
+    address, sig = signChallenge(challenge)
 
+    # Recover the address from the signed message and verify
     w3 = Web3()
-
-    return w3.eth.account.recover_message( challenge , signature=sig ) == address
+    return w3.eth.account.recover_message(challenge, signature=sig) == address
+    
 
 if __name__ == '__main__':
     """
